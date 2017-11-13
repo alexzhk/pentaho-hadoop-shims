f6a10bed6a18f33528c65ff50b7c473ce9db7185
Aliaksandr Zhuk <aliaksandr_zhuk@epam.com>
2017-11-13 16:31:46 +0300

    [BAD-1160]-make pig working with karaf

--- common/common-shim/src/main/java/org/pentaho/hadoop/shim/common/CommonPigShim.java
+++ common/common-shim/src/main/java/org/pentaho/hadoop/shim/common/CommonPigShim.java
@@ -1,6 +1,12 @@
 /*******************************************************************************
  *
  * Pentaho Big Data
+ * <p>
+ * Copyright (C) 2002-2015 by Pentaho : http://www.pentaho.com
+ * <p>
+ * ******************************************************************************
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  *
  * Copyright (C) 2002-2017 by Hitachi Vantara : http://www.pentaho.com
  *
@@ -22,26 +28,92 @@
 
 package org.pentaho.hadoop.shim.common;
 
+import com.google.common.collect.Multimaps;
+import org.antlr.runtime.CommonTokenStream;
+import org.apache.commons.io.FileUtils;
 import org.apache.pig.ExecType;
 import org.apache.pig.backend.hadoop.datastorage.ConfigurationUtil;
+import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigMapReduce;
+import org.apache.pig.impl.PigContext;
 import org.apache.pig.impl.util.PropertiesUtil;
 import org.apache.pig.tools.parameters.ParameterSubstitutionPreprocessor;
 import org.apache.pig.tools.parameters.ParseException;
+import org.apache.tools.bzip2r.BZip2Constants;
+import org.codehaus.jackson.annotate.JsonPropertyOrder;
+import org.codehaus.jackson.map.annotate.JacksonStdImpl;
+import org.osgi.framework.BundleContext;
 import org.pentaho.hadoop.shim.ShimVersion;
 import org.pentaho.hadoop.shim.api.Configuration;
 import org.pentaho.hadoop.shim.spi.PigShim;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringWriter;
+import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
 public abstract class CommonPigShim implements PigShim {
   private static final String[] EMPTY_STRING_ARRAY = new String[ 0 ];
+
+
+  private BundleContext bundleContext;
+
+  public BundleContext getBundleContext() {
+    return bundleContext;
+  }
+
+  public void setBundleContext( BundleContext bundleContext ) {
+    this.bundleContext = bundleContext;
+  }
+
+  private enum ExternalPigJars {
+
+    PIG( "pig" ),
+    AUTOMATON( "automaton" ),
+    ANTLR( "antlr-runtime" ),
+    JACKSON_CORE( "jackson-core-asl" ),
+    JACKSON_MAPPER( "jackson-mapper-asl" ),
+    JODATIME( "joda-time" );
+
+    private final String jarName;
+
+    ExternalPigJars( String jarName ) {
+      this.jarName = jarName;
+    }
+
+    public String getJarName() {
+      return jarName;
+    }
+
+  }
+
+  public void addExternalJarsToPigContext( PigContext pigContext ) throws MalformedURLException {
+    for ( ExternalPigJars externalPigJars : ExternalPigJars.values() ) {
+      String externaljarPath = "";
+      externaljarPath = getExternalJarAbsolutePath( externalPigJars.getJarName() );
+      pigContext.addJar( externaljarPath );
+    }
+  }
+
+  private String getExternalJarAbsolutePath( String jarName ) {
+    Iterator<File> filesIterator =
+      FileUtils.iterateFiles( new File( new File( bundleContext.getBundle().getDataFile( "/" ).getParent() ).getParent()
+          + File.separator ),
+        new String[] { "jar" }, true );
+    String jarPath = "";
+
+    while ( filesIterator.hasNext() ) {
+      File file = filesIterator.next();
+      String name = file.getName();
+      if ( name.startsWith( jarName ) ) {
+        jarPath = file.getAbsolutePath();
+        return jarPath;
+      }
+    }
+
+    return jarPath;
+  }
 
   @Override
   public ShimVersion getVersion() {
@@ -80,7 +152,7 @@ public abstract class CommonPigShim implements PigShim {
    * @return Type of execution for mode
    */
   protected ExecType getExecType( ExecutionMode mode ) {
-    switch ( mode ) {
+    switch( mode ) {
       case LOCAL:
         return ExecType.LOCAL;
       case MAPREDUCE:
