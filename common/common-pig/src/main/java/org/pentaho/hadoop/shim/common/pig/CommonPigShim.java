60ab07121093a028a08941e65d17e905303e0633
Aliaksandr Zhuk <aliaksandr_zhuk@epam.com>
2017-11-17 17:00:00 +0300

    [BAD-1167]-make pig working in new structure

--- common/common-pig/src/main/java/org/pentaho/hadoop/shim/common/pig/CommonPigShim.java
+++ common/common-pig/src/main/java/org/pentaho/hadoop/shim/common/pig/CommonPigShim.java
@@ -1,53 +1,41 @@
 /*******************************************************************************
- *
  * Pentaho Big Data
  * <p>
- * Copyright (C) 2002-2015 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2017 by Hitachi Vantara : http://www.pentaho.com
  * <p>
  * ******************************************************************************
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- *
- * Copyright (C) 2002-2017 by Hitachi Vantara : http://www.pentaho.com
- *
- *******************************************************************************
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  ******************************************************************************/
 
 package org.pentaho.hadoop.shim.common.pig;
 
-import com.google.common.collect.Multimaps;
-import org.antlr.runtime.CommonTokenStream;
 import org.apache.commons.io.FileUtils;
 import org.apache.pig.ExecType;
 import org.apache.pig.backend.hadoop.datastorage.ConfigurationUtil;
-import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigMapReduce;
 import org.apache.pig.impl.PigContext;
 import org.apache.pig.impl.util.PropertiesUtil;
 import org.apache.pig.tools.parameters.ParameterSubstitutionPreprocessor;
 import org.apache.pig.tools.parameters.ParseException;
-import org.apache.tools.bzip2r.BZip2Constants;
-import org.codehaus.jackson.annotate.JsonPropertyOrder;
-import org.codehaus.jackson.map.annotate.JacksonStdImpl;
 import org.osgi.framework.BundleContext;
 import org.pentaho.hadoop.shim.ShimVersion;
 import org.pentaho.hadoop.shim.api.Configuration;
 import org.pentaho.hadoop.shim.common.ShimUtils;
 import org.pentaho.hadoop.shim.spi.PigShim;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Iterator;
@@ -163,5 +151,4 @@ public abstract class CommonPigShim implements PigShim {
         throw new IllegalStateException( "unknown execution mode: " + mode );
     }
   }
-
 }
--- common/common-pig/src/main/java/org/pentaho/hadoop/shim/common/pig/PigShimImpl.java
+++ common/common-pig/src/main/java/org/pentaho/hadoop/shim/common/pig/PigShimImpl.java
@@ -1,32 +1,29 @@
-/*! ******************************************************************************
- *
- * Pentaho Data Integration
- *
- * Copyright (C) 2002-2014 by Pentaho : http://www.pentaho.com
- *
- *******************************************************************************
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with
+/*******************************************************************************
+ * Pentaho Big Data
+ * <p>
+ * Copyright (C) 2002-2017 by Hitachi Vantara : http://www.pentaho.com
+ * <p>
+ * ******************************************************************************
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  ******************************************************************************/
+
 package org.pentaho.hadoop.shim.common.pig;
 
 import org.apache.pig.PigServer;
+import org.apache.pig.impl.PigContext;
 import org.apache.pig.tools.grunt.GruntParser;
 import org.pentaho.hadoop.shim.spi.PigShim;
 
 import java.io.IOException;
-import java.io.StringReader;
+//$import java.io.StringReader;
 import java.util.Properties;
 
 /**
@@ -37,23 +34,17 @@ public class PigShimImpl extends CommonPigShim {
   @Override
   public int[] executeScript( String pigScript, PigShim.ExecutionMode mode, Properties properties )
     throws IOException, org.apache.pig.tools.pigscript.parser.ParseException {
-    ClassLoader cl = Thread.currentThread().getContextClassLoader();
-    Thread.currentThread().setContextClassLoader( getClass().getClassLoader() );
-    try {
-
-      PigServer pigServer = new PigServer( getExecType( mode ), properties );
-      //#if shim_type=="CDH"
-        GruntParser grunt = new GruntParser( new StringReader( pigScript ) );
-        grunt.setInteractive( false );
-        grunt.setParams( pigServer );
-      //#else
-        //$GruntParser grunt = new GruntParser( new StringReader( pigScript ), pigServer );
-        //$grunt.setInteractive( false );
-      //#endif
-      return grunt.parseStopOnError( false );
-    } finally {
-      Thread.currentThread().setContextClassLoader( cl );
-    }
+    GruntParser grunt = null;
+    PigContext pigContext = new PigContext( getExecType( mode ), properties );
+    addExternalJarsToPigContext( pigContext );
+    PigServer pigServer = new PigServer( pigContext );
+    //#if shim_type=="CDH"
+    //$grunt = new GruntParser( new StringReader( pigScript ) );
+    //$grunt.setParams( pigServer );
+    //#else
+    //$grunt = new GruntParser( new StringReader( pigScript ), pigServer );
+    //#endif
+    grunt.setInteractive( false );
+    return grunt.parseStopOnError( false );
   }
-
 }
